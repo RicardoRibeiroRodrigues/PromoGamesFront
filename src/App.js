e9bc18b18985139a2d3f1b6637648e86da66eb6a
Ricardo Ribeiro Rodrigues <riywar@gmail.com>
2022-06-08 15:43:47 -0300

    Simulado feito

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
   const [favorites, setFavorites] = useState([]);
   const [onlyFavorites, setOnlyFavorites] = useState(false);
   const [timeoutTimer, setTimeoutTimer] = useState(false);
+  const [mensagem, setMensagem] = useState("");
 
   const isFavorite = (dealId) => {
     for (let favorite of favorites) {
@@ -36,8 +37,19 @@ function App() {
       // .get("http://127.0.0.1:8000/api/favorites") // local
       .get("https://afternoon-stream-42339.herokuapp.com/api/favorites")
       .then((res) => setFavorites(res.data));
-    // 
+    // Fazendo o simulado
+    axios
+      .get(`https://enigmatic-bayou-56424.herokuapp.com/ricardorr7/token`)
+      .then((res) => {
+        const token = res.data.token;
+        axios
+          .post("https://enigmatic-bayou-56424.herokuapp.com/ricardorr7/message", { token })
+          .then((res) => {
+            if (!mensagem)
+              setMensagem(res.data.mensagem)
+          });
 
+      });
 
   }, [filters]);
 
@@ -58,9 +70,33 @@ function App() {
       </div>);
   }
 
+  const MensagemAleatoria = () => {
+    return (
+      <div style={{
+        position: 'absolute',
+        color: 'rgba(255, 255, 255, 0.8)',
+        zIndex: 1000,
+        left: '50%',
+        transform: "translate(-50%, 0)",
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        background: "rgba(1, 1, 1, 0.5)",
+        boxShadow: "0 8px 32px 0 rgba(31, 38, 135, 0.37)",
+        backdropFilter: "blur(4px)",
+        borderRadius: "10px",
+        border: "1px solid rgba(255, 255, 255, 0.18)",
+        width: '50%',
+        padding: '1rem',
+      }}>
+        <h1 style={{ flexWrap: "wrap" }}>{mensagem}</h1>
+      </div >
+    );
+  }
+
   return (
     <div>
       <SiteAppBar setFilters={setFilters} filters={filters} />
+      <MensagemAleatoria></MensagemAleatoria>
       <div className="flex-column">
         <ExpansibleMenu stores={stores} setFilters={setFilters}
           onlyFavorites={{ 'state': onlyFavorites, 'setter': setOnlyFavorites }} />
